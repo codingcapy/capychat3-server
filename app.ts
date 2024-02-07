5cb233efc0dbe11aced46c11e7caabbbf1ab8950
Paul K <spkim0921@gmail.com>
2024-02-06 23:00:04 -0800

    5th commit
    implementing web socket

--- app.ts
+++ app.ts
@@ -1,36 +1,44 @@
 
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-import users from "./routes/users"
-import connectDB from "./connect"
-import user from "./routes/user"
-import friends from "./routes/friends"
-import chats from "./routes/chats"
-import messages from "./routes/messages"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import http from "http";
+import { Server as SocketServer } from "socket.io";
+import users from "./routes/users";
+import connectDB from "./connect";
+import user from "./routes/user";
+import friends from "./routes/friends";
+import chats from "./routes/chats";
+import messages from "./routes/messages";
 
 dotenv.config()
-const app = express()
-const port = 3333
+const app = express();
+const port = 3333;
 
-app.use(express.json())
-app.use(cors())
-app.get("/", (req, res) => res.send("welcome"))
-app.use("/api/users", users)
-app.use("/api/user", user)
-app.use("/api/user/friends", friends)
-app.use("/api/chats", chats)
-app.use("/api/messages", messages)
+app.use(express.json());
+app.use(cors());
+const server = http.createServer(app);
+const io = new SocketServer(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+app.get("/", (req, res) => res.send("welcome"));
+app.use("/api/users", users);
+app.use("/api/user", user);
+app.use("/api/user/friends", friends);
+app.use("/api/chats", chats);
+app.use("/api/messages", messages);
 
 async function start() {
     try {
-        await connectDB(process.env.MONGO_URI)
-        console.log("Connected to database.")
-        app.listen(port, () => console.log(`Server listening on port: ${port}`))
+        await connectDB(process.env.MONGO_URI);
+        console.log("Connected to database.");
+        app.listen(port, () => console.log(`Server listening on port: ${port}`));
     }
     catch (err) {
-        console.log(err)
+        console.log(err);
     }
 }
 
-start()
+start();
